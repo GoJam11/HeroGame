b20d17e8e1ddc7bb34ae3070d71766effeed7509
Guo <iyilan@qq.com>
2019-04-08 08:31:56 +0800

    no

--- assets/script/Main.js
+++ assets/script/Main.js
@@ -20,8 +20,10 @@ cc.Class({
             default: null,
             type: cc.Node
         },
-        pool:[cc.Node]
-        
+        pool: [cc.Node],
+        distance: [],
+        timer: null
+
         // foo: {
         //     // ATTRIBUTES:
         //     default: null,        // The default value will be used only when the component attaching
@@ -46,13 +48,26 @@ cc.Class({
     start() {
         this.myHero = cc.find("bg/MyHero", this.node)
         this.hero = cc.find("bg/Hero", this.node)
-this.pool.push(this.myHero)
-this.pool.push(this.hero)
-        console.log(this.pool)
+        this.pool.push(this.myHero)
+        this.pool.push(this.hero)
+
     },
 
     update(dt) {
         //attack
-        
+        this.pool.forEach(el => {
+            this.pool.forEach(ele => {
+                let _x = el.x - ele.x
+                let _y = el.y - ele.y
+                let _d = Math.sqrt(Math.pow(_x, 2) + Math.pow(_y, 2))
+                if (_d < 150 && el != ele) {
+                    console.log("attack")
+                        //如果计时器=0或>0.3允许动画并开始计时，如果在0-0.3之间不执行动画
+                    this.timer = new Date()
+                    cc.tween(el).to(0.3, { rotation: 30 }).start()
+                }
+
+            })
+        });
     },
 });
